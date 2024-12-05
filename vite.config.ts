f7b0b853e730c9396a0a3fb4aa3e1fdf87e8aec3
phe94br1 <bubble.augusto@gmail.com>
2024-12-05 20:49:43 -0300

    revert: Remover configurações do GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/vite-creation-wizard/',
-  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
-    strictPort: true,
-    hmr: {
-      clientPort: 443
-    }
   },
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    sourcemap: true,
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
-}))
+}));
